5a3be524547cb6b52125be709054200d25a85733
PhilipTKC <philipthongkhamchanh@gmail.com>
2023-05-11 21:46:36 +1200

    Update

--- dist/index.js
+++ dist/index.js
@@ -5,7 +5,7 @@ const nanoid_1 = require("nanoid");
 * This plugin assigns a random data-key attribute to all headings.
 * This plugin also adds data-key-content to all sibling elements belonging to the same heading.
 */
-const plugin = (md) => {
+const dataAttributePlugin = (md) => {
     return md.renderer.rules.heading_open = (tokens, idx, options, env, self) => {
         tokens[idx].attrPush(['data-key', (0, nanoid_1.nanoid)(8)]);
         let nextHeaderIdx = idx + 1;
@@ -15,10 +15,10 @@ const plugin = (md) => {
         for (let i = idx + 1; i < nextHeaderIdx; i++) {
             if (tokens[i].type === 'text')
                 continue;
-            const parentSlug = tokens[idx].attrs.filter(attr => attr[0] === 'data-key')[0][1];
-            tokens[i].attrPush(['data-key-content', parentSlug]);
+            const parentKey = tokens[idx].attrs.filter(attr => attr[0] === 'data-key')[0][1];
+            tokens[i].attrPush(['data-key-content', parentKey]);
         }
         return self.renderToken(tokens, idx, options);
     };
 };
-exports.default = plugin;
+exports.default = dataAttributePlugin;
